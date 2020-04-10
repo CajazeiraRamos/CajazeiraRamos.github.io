6d430a6f883318e33ff25dbc89d03e40e6f3b355
cajazeiraramos <alexandreramos@lia.ufc.br>
2020-04-10 09:52:42 -0300

    index5

---
+++ Covid19/js/Covid19-Brasil.js
@@ -0,0 +1,32 @@
+let populacaoUF = d3.scale.ordinal()
+	.domain(["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"])
+	.range(['881935','3337357','845731','4144597','14873064','9132078','3015268','4018650','7018354','7075181','3484466','2778986','21168791','8602865','4018127','11433957','9557071','3273227','17264943','3506853','11377239','1777225','605761','7164788','45919049','2298696','1572866']),
+
+nomeUF = d3.scale.ordinal()
+	.domain(["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"])
+	.range(['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']),
+
+siglaRegiao = d3.scale.ordinal()
+	.domain(["Sudeste", "Nordeste", "Sul", "Centro-Oeste", "Norte"])
+	.range(['SE','NE','S','CO','N']),
+
+colorRegiao = d3.scale.ordinal()
+	.domain(["Sudeste", "Nordeste", "Sul", "Centro-Oeste", "Norte"])
+	.range(['#b3e2cd','#fdcdac','#cbd5e8','#f4cae4','#e6f5c9']);
+
+
+let centroMapa = [-13, -65],
+	zoomMapa = 4;
+
+let Mapa = L.map('divMapa').setView(centroMapa, zoomMapa);
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {maxZoom: 18, attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`})
+.addTo(Mapa);
+
+
+let dataAtual, escala=0, controleUF_RG=0; 
+
+
+function trocaEscala(){
+	escala = !escala;
+}
